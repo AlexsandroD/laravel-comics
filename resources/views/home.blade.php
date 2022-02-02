611fe23787e68c644cea22bac13e52d4f3214bc8
AlexsandroD <adrdra88@gmail.com>
2022-02-02 18:59:47 +0100

    cards

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,1 +1,19 @@
+@extends('layouts.base')
 
+@section('pageTitle')
+
+@endsection
+
+@section('pageContent')
+    <div class="container">
+        <div class="row">
+            @foreach ($comics as $comic )
+                <div class="card">
+                    <img src="{{$comic['thumb']}}" alt="">
+                    <div class="title">{{$comic['series']}}</div>
+                </div>
+            @endforeach
+            <div class="btn"><a href="#">Load More</a></div>
+        </div>
+    </div>
+@endsection
